3b71900f0ea3afb2624ce6e49d0032a4c9a65c93
majes <liones2014@mail.ru>
2021-01-16 16:10:33 +0700

    vueForm

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+
+  <div>
+    <Form/>
+
+  </div>
+</template>
+
+<script>
+
+import Form from "@/components/Form";
+
+export default {
+  name: 'App',
+  components: {
+    Form
+
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
